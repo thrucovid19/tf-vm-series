1b374e068cd659584158d3d013d67bb9f5292dcb
thrucvoid19 <thrucovid19@gmail.com>
2020-08-05 12:19:06 -0500

    Second commit

--- example/test.tf
+++ example/test.tf
@@ -14,12 +14,11 @@ resource "aws_key_pair" "thrucovid19" {
 }
 
 module "vmseries" {
-  source         = "github.com/thrucovid19/tf-vm-series"
-  name           = "Test"
-  environment    = "testing"
-  enable_ha      = true
-  key_name       = aws_key_pair.thrucovid19.key_name
-  mgmt_subnet    = "192.168.1.0/24"
+  source      = "github.com/thrucovid19/tf-vm-series"
+  name        = "Test"
+  environment = "testing"
+  key_name    = aws_key_pair.thrucovid19.key_name
+  mgmt_subnet = "192.168.1.0/24"
 }
 
 output "vm-series-eip" {
--- variables.tf
+++ variables.tf
@@ -1,14 +1,63 @@
-variable instance_type {
-  description = "Instance type for VM-Series"
-  default = "m4.2xlarge"
+variable "name" {
+  default     = "Default"
+  type        = string
+  description = "Name of the VPC"
 }
 
-variable vmseries_version {
-  description = "Mainline version for VM-Series. Does not define a specific release number."
-  default = "9.1"
+variable "environment" {
+  type        = string
+  description = "Name of environment: ex. stg, prod, dev"
 }
 
-variable aws_key {
+variable "tags" {
+  default     = {}
+  type        = map(string)
+  description = "Extra tags to attach to the VPC resources"
+}
+
+variable "region" {
+  default     = "us-east-1"
+  type        = string
+  description = "Region of the VPC"
+}
+
+variable "mgmt_subnet" {
+  default     = "192.168.1.0/24"
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "cidr_block" {
+  default     = "10.0.0.0/16"
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "availability_zones" {
+  default     = "us-east-1a", "us-east-1b"
+  type        = string
+  description = "List of availability zones"
+}
+
+variable "public_subnet_cidr_block" {
+  default     = "10.0.0.0/24"
+  type        = string
+  description = "Public subnet CIDR block"
+}
+
+variable "management_subnet_cidr_block" {
+  default     = "10.0.10.0/24"
+  type        = string
+  description = "Management subnet CIDR block"
+}
+
+variable "private_subnet_cidr_block" {
+  default     = "10.0.1.0/24"
+  type        = string
+  description = "Private subnet CIDR block"
+}
+
+variable key_name {
 }
 
 variable instance_name {
